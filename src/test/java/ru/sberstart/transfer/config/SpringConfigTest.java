c97cad9bda3e816fdfad039a2cd0119c54edc1fd
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2021-01-18 12:24:49 +0300

    g

--- src/test/java/ru/sberstart/transfer/config/SpringConfigTest.java
+++ src/test/java/ru/sberstart/transfer/config/SpringConfigTest.java
@@ -7,26 +7,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan("ru.sberstart.transfer")
-@EnableWebMvc
-public class SpringConfig implements WebMvcConfigurer {
-
+public class SpringConfigTest {
     private final ApplicationContext applicationContext;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfigTest(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
@@ -47,34 +44,9 @@ public class SpringConfig implements WebMvcConfigurer {
         return templateEngine;
     }
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
-
-//    @Bean
-//    public JdbcTemplate jdbcTemplate() {
-//        return new JdbcTemplate(ru.sberstart.transfer.config.DataSource.getDataSource());
-//    }
-
     @Bean
     public DataSource dataSource() throws IOException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-//        Properties properties = new Properties();
-//        properties.load(Class.class.getResourceAsStream("/application.properties"));
-//
-//        System.out.println(properties.getProperty("db.driver.name"));
-//        System.out.println(properties.getProperty("db.url"));
-//        System.out.println(properties.getProperty("db.user"));
-//        System.out.println(properties.getProperty("db.password"));
-//
-//        dataSource.setDriverClassName(properties.getProperty("db.driver.name"));
-//        dataSource.setUrl(properties.getProperty("db.url"));
-//        dataSource.setUsername(properties.getProperty("db.user"));
-//        dataSource.setPassword(properties.getProperty("db.password"));
 
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/transfer");
@@ -94,8 +66,5 @@ public class SpringConfig implements WebMvcConfigurer {
         return new JdbcTemplate(dataSource());
     }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource){
-        return new JdbcTemplate(dataSource);
-    }
+
 }
--- src/test/java/ru/sberstart/transfer/dao/CardDaoImplTest.java
+++ src/test/java/ru/sberstart/transfer/dao/CardDaoImplTest.java
@@ -2,18 +2,35 @@ package ru.sberstart.transfer.dao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
+import ru.sberstart.transfer.config.SpringConfigTest;
 import ru.sberstart.transfer.dao.implement.CardDaoImpl;
 
+import javax.sql.DataSource;
 
-@Transactional
-class CardDaoImplTest extends Abstract{
+@DirtiesContext
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfigTest.class)
+class CardDaoImplTest {
     @Autowired
     private CardDaoImpl cardDaoImpl;
 
     @BeforeEach
     void setUp() {
+        DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:data.sql")
+                .build();
     }
 
     @Test
